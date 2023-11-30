313ddc08673a23e313adb4c4d3826951b79f0b33
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-11-30 17:59:25 -0500

    Initial Components and basic styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'start',
-        alignItems: 'start',
-        fontSize: '30px',
-        color: '#010101',
-      }}
-    >
-      <h1>Hola Tarea de GoIT</h1>
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'start',
+          alignItems: 'start',
+          fontSize: '30px',
+          color: '#010101',
+        }}
+      >
+        <h1>Hola {this.state.name}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
